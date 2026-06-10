364fc9f942638031a5390978e5674cb56c76423f
Julian Kuelshammer <julian.kuelshammer@math.uu.se>
2026-06-10 10:57:47 +0200

    export lemmas from lt1 and lt2

--- Reedy/Reedy/SimplexCategory.lean
+++ Reedy/Reedy/SimplexCategory.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joël Riou
+Authors: Joël Riou, Julian Külshammer
 -/
 module
 
@@ -20,6 +20,21 @@ open CategoryTheory MorphismProperty
 
 namespace SimplexCategory
 
+lemma identities_of_epi_of_len_eq {x y : SimplexCategory} (f : x ⟶ y) [Epi f]
+  (hlen : x.len = y.len) : identities SimplexCategory f := by
+  have heq := SimplexCategory.ext_iff.mpr hlen
+  subst heq
+  have := eq_id_of_epi f
+  rw [this]
+  exact ofHoms.mk _
+
+lemma identities_of_mono_of_len_eq {x y : SimplexCategory} (f : x ⟶ y) [Mono f]
+  (hlen : x.len = y.len) : identities SimplexCategory f := by
+  have heq := SimplexCategory.ext_iff.mpr hlen
+  subst heq
+  have := eq_id_of_mono f
+  rw [this]
+  exact ofHoms.mk _
 
 -- C.4.4
 -- claim https://github.com/joelriou/reedy/issues/22 if working on this
@@ -30,24 +45,11 @@ def reedyStructure :
   deg := len
   lt₁ f hepi hnonid := by
     haveI := (epimorphisms.iff f).mp hepi
-    apply lt_of_le_of_ne (len_le_of_epi f)
-    intro h
-    have heq := SimplexCategory.ext_iff.mpr h
-    subst heq
-    have := eq_id_of_epi f
-    apply hnonid
-    rw [this]
-    exact ofHoms.mk _
+    exact lt_of_le_of_ne (len_le_of_epi f) (fun h ↦ hnonid (identities_of_epi_of_len_eq f h.symm))
+
   lt₂ f hmono hnonid := by
     haveI := (monomorphisms.iff f).mp hmono
-    apply lt_of_le_of_ne (len_le_of_mono f)
-    intro h
-    have heq := SimplexCategory.ext_iff.mpr h
-    subst heq
-    have := eq_id_of_mono f
-    apply hnonid
-    rw [this]
-    exact ofHoms.mk _
+    exact lt_of_le_of_ne (len_le_of_mono f) (fun h ↦ hnonid (identities_of_mono_of_len_eq f h))
   nonempty_unique := sorry
 
 end SimplexCategory
