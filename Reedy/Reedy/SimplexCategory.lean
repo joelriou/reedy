8b6e29288abea950a9eb384b3a12b4ea4925fbfd
Julian Kuelshammer <julian.kuelshammer@math.uu.se>
2026-06-10 14:07:20 +0200

    update version of lt1 and lt2 and sorry-free nonempty_unique

--- Reedy/Reedy/SimplexCategory.lean
+++ Reedy/Reedy/SimplexCategory.lean
@@ -20,22 +20,6 @@ open CategoryTheory MorphismProperty
 
 namespace SimplexCategory
 
-lemma identities_of_epi_of_len_eq {x y : SimplexCategory} (f : x ⟶ y) [Epi f]
-  (hlen : x.len = y.len) : identities SimplexCategory f := by
-  have heq := SimplexCategory.ext_iff.mpr hlen
-  subst heq
-  have := eq_id_of_epi f
-  rw [this]
-  exact ofHoms.mk _
-
-lemma identities_of_mono_of_len_eq {x y : SimplexCategory} (f : x ⟶ y) [Mono f]
-  (hlen : x.len = y.len) : identities SimplexCategory f := by
-  have heq := SimplexCategory.ext_iff.mpr hlen
-  subst heq
-  have := eq_id_of_mono f
-  rw [this]
-  exact ofHoms.mk _
-
 -- C.4.4
 -- claim https://github.com/joelriou/reedy/issues/22 if working on this
 def reedyStructure :
@@ -43,13 +27,40 @@ def reedyStructure :
 -- the proof should follow from ingredients in the file
 -- `Mathlib.AlgebraicTopology.SimplexCategory.Basic`
   deg := len
-  lt₁ f hepi hnonid := by
-    haveI := (epimorphisms.iff f).mp hepi
-    exact lt_of_le_of_ne (len_le_of_epi f) (fun h ↦ hnonid (identities_of_epi_of_len_eq f h.symm))
-
-  lt₂ f hmono hnonid := by
-    haveI := (monomorphisms.iff f).mp hmono
-    exact lt_of_le_of_ne (len_le_of_mono f) (fun h ↦ hnonid (identities_of_mono_of_len_eq f h))
-  nonempty_unique := sorry
+  lt₁ := by
+    intro n m f _ hf
+    refine lt_of_le_of_ne (len_le_of_epi f) (fun h ↦ ?_)
+    obtain rfl : m = n := SimplexCategory.ext_iff.mpr h
+    obtain rfl := eq_id_of_epi f
+    exact hf ⟨_⟩
+  lt₂  := by
+    intro n m f _ hf
+    refine lt_of_le_of_ne (len_le_of_mono f) (fun h ↦ ?_)
+    obtain rfl : m = n := SimplexCategory.ext_iff.mpr h.symm
+    obtain rfl := eq_id_of_mono f
+    exact hf ⟨_⟩
+  nonempty_unique {x y} f := by
+    refine ⟨?_, ?_⟩
+    · haveI : CategoryTheory.Limits.HasStrongEpiMonoFactorisations SimplexCategory:= by
+        infer_instance
+      obtain ⟨F⟩ := this
+      letI t := (F f).some
+      exact ⟨t.I, t.e, t.m, t.fac, by infer_instance, by infer_instance⟩
+    haveI : Subsingleton
+      ((epimorphisms SimplexCategory).MapFactorizationData (monomorphisms SimplexCategory) f) := by
+      refine ⟨?_⟩
+      rintro ⟨z1, p1, i1, hfac1, hepi1, hmono1⟩ ⟨z2, p2, i2, hfac2, hepi2, hmono2⟩
+      simp [monomorphisms.iff] at hmono1 hmono2
+      simp [epimorphisms.iff] at hepi1 hepi2
+      have hz1 := (@image_eq _ _ _ _ _ _ _ _ hfac1).symm
+      have hz2 := (@image_eq _ _ _ _ _ _ _ _ hfac2).symm
+      subst hz1 hz2
+      have hi : i1 = i2 := by
+        rw [← image_ι_eq hfac1, image_ι_eq hfac2]
+      have hp : p1 = p2 := by
+        rw [← factorThruImage_eq hfac1, factorThruImage_eq hfac2]
+      subst hi hp
+      rfl
+    exact fun _ ↦ Subsingleton.elim _ _
 
 end SimplexCategory
