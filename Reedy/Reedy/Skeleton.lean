68eed02a68c4edcf19fccdb287028b2a21f0db0b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-06-12 20:37:08 +0200

    removed partially wrong stuff

--- Reedy/Reedy/Skeleton.lean
+++ Reedy/Reedy/Skeleton.lean
@@ -25,28 +25,6 @@ namespace CategoryTheory
 
 open HomotopicalAlgebra Opposite Limits FunctorToTypes
 
-namespace Limits
-
-variable (C : Type u) [Category.{v} C] (D : Type*) [Category* D]
-  [HasColimitsOfSize.{v, max u v} D]
-
-noncomputable abbrev toArrowCompWeightedColim₂LeibnizPushout :
-    Over (yoneda (C := C)) ⥤ Arrow (C ⥤ D) ⥤ Arrow (C ⥤ D) :=
-  letI : HasColimitsOfSize.{0, 0} D := hasColimitsOfSizeShrink D
-  Over.toArrow ⋙ weightedColim₂.leibnizPushout
-
-instance (K : Type*) [Category* K] [HasColimitsOfShape K D] :
-    PreservesColimitsOfShape K (toArrowCompWeightedColim₂LeibnizPushout C D) := sorry
-
-instance {α : Type*} [LinearOrder α] [OrderBot α] [SuccOrder α] [WellFoundedLT α]
-    [HasIterationOfShape α D] :
-    PreservesWellOrderContinuousOfShape α (toArrowCompWeightedColim₂LeibnizPushout C D) where
-  preservesColimitsOfShape m hm := by
-    have := hasColimitsOfShape_of_isSuccLimit D m hm
-    infer_instance
-
-end Limits
-
 variable {C : Type u} [SmallCategory C] {W₁ W₂ : MorphismProperty C}
   [W₁.IsMultiplicative] [W₂.IsMultiplicative]
   {α : Type*} [LinearOrder α] [OrderBot α] [SuccOrder α] [WellFoundedLT α]
@@ -59,17 +37,6 @@ variable (r : ReedyStructure W₁ W₂ α) {D : Type*} [Category D]
 
 noncomputable def skFunctor : α ⥤ (C ⥤ D) ⥤ C ⥤ D :=
   r.monotone_skYoneda.functor ⋙ Subfunctor₂.toFunctorFunctor yoneda ⋙ weightedColim₂
-
-variable (C D) in
-noncomputable abbrev toArrowCompWeightedColim₂LeibnizPushout :
-    Over (yoneda (C := C)) ⥤ Arrow (C ⥤ D) ⥤ Arrow (C ⥤ D) :=
-  Over.toArrow ⋙ weightedColim₂.leibnizPushout
-
-variable [NoMaxOrder α]
-
--- hopefully, up to isomorphisms, this should give the relative skeleton filtration
--- of a morphism (and functoriality in this map)
-#check r.relativeCellComplexOver.map (toArrowCompWeightedColim₂LeibnizPushout C D)
 
 end ReedyStructure
 
